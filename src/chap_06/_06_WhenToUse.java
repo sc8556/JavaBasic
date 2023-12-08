e2f268582b31935e5e924a3835e80922c719d517
JAY <sc8556@naver.com>
2023-12-08 10:28:55 +0900

    2023.12.08 깃허브 연동 및 브랜치 생성, 업로드 테스트

---
+++ src/chap_06/_06_WhenToUse.java
@@ -0,0 +1,11 @@
+package chap_06;
+
+public class _06_WhenToUse {
+    public static void main(String[] args) {
+        // 메소드가 필요한 이유, 언제 사용하는지
+        int result = 1; // result 변수 선언
+        for (int i = 0; i < 2; i++) {
+            result *= 2;
+        }
+    }
+}
---
+++ src/chap_06/gitTest.java
@@ -0,0 +1,7 @@
+package chap_06;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("깃 허브 브랜치 생성 및 연동 테스트 입니다.");
+    }
+}
